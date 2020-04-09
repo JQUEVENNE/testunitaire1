6f273339d8f936c34872dff51beaeed4c595d5c2
jquevenne <j.quevenne@ecole-ipssi.net>
2020-04-09 12:26:39 +0200

    exo1ko

--- public/index.php
+++ public/index.php
@@ -3,28 +3,7 @@
 require('../vendor/autoload.php');
 
 use App\Calculatrice;
-Use Test\CalculatriceTest;
-
-function d(...$values) {
-    foreach($values as $value) {
-        echo '<pre>';
-        var_dump($value);
-        echo '</pre>';
-    }
-}
-
-function dd(...$values) {
-    d(...$values);
-    die;
-}
+Use tests\CalculatriceTest;
 
 
-
-$calc = new Calculatrice();
-
-d($calc->setA(12)->setB(5)->add());
-d($calc->setA(76)->setB(3)->div());
-
-$test = new CalculatriceTest();
-$test->testAdd();
 ?>
